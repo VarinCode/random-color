570d2b9e11d150aa84acb5af6e70bc7d6627cd2f
Varin Saipanya <94356640+Varin471@users.noreply.github.com>
2022-06-05 21:17:38 +0700

    Add files via upload

---
+++ src/style/style.tsx
@@ -0,0 +1,74 @@
+export const logoinformation = {
+    fontSize:'20px' , 
+    position: 'relative' , 
+    bottom:'3px'
+};
+
+export const vent = {
+    marginTop: '100px'
+};
+
+export const fontnavbar= {
+    fontFamily: 'Akshar'
+};
+
+export const fontfooter= {
+    fontFamily: 'Akshar'
+};
+
+export const fontthai= {
+    fontFamily: 'Itim'
+};
+
+// important 
+
+export const render = {
+    position: 'relative',
+    top: '70px',
+    textalign: 'center',
+    color: 'rgb(255, 255, 255)',
+    textShadow: '2px 2px 4px rgb(228, 31, 31)',
+    padding: '15px'
+}
+
+export const renderbox = {
+    fontFamily: 'Itim'
+};
+
+export const frame = {
+    backgroundImage: 'url("https://img.freepik.com/free-photo/dark-black-chalk-board-school-background-texture_148157-92.jpg")',
+    display: 'block',
+    borderBottomLeftRadius: '50px',
+    borderBottomRightRadius: '50px',
+    borderTopLeftRadius: '50px',
+    borderTopRightRadius: '50px',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    width: '70%',
+    height: '200px',
+    boxShadow: 'rgba(0, 0, 0, 0.3) 0px 19px 38px ,  rgba(0, 0, 0, 0.22) 0px 15px 12px'
+};
+
+
+export const header = {
+    textShadow: '2px 2px 4px rgb(51, 51, 51)'
+};
+
+export const show ={
+        fontFamily: 'Itim',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: '30vh',
+        overflow: 'hidden',
+        maxWidth: '100%',
+        margin: '0 auto',
+        boxShadow: '0 20px 60px rgba(0, 0, 0, 0.381)',
+        backgroundColor: 'rgb(255, 255, 255)'
+};
+
+
+
+// export group
+// export { logoinformation , vent , fontnavbar , fontfooter , renderbox , fontthai , render , frame , header} 
