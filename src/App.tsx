4254a1801aa7b4dea1e9e908a0b95dc48b453853
VarinCode <94356640+VarinCode@users.noreply.github.com>
2022-10-09 19:18:12 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,25 @@
-import React from 'react'
+// import react
+import React, { Fragment } from 'react';
 
-// lib
+// import lib
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import 'sweetalert2/dist/sweetalert2.min.css';
 import 'sweetalert2/dist/sweetalert2.min.js';
 import 'remixicon/fonts/remixicon.css';
 
-// component (default)
+// import component(default)
 import  Color  from './component/color';
 import Navbar from './component/navbar';
 import Footer from './component/footer';
 export default class App extends React.Component {
 render(): React.ReactNode {
      return (  
-          <React.Fragment>
+          <Fragment>
             <Navbar/>
               <Color />
             <Footer />
-          </React.Fragment>
+          <Fragment>
     )
   }
 }
